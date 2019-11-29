00b71de9308df5534cbab0a7ddc7cef3ad1d401d
Oscar Chen <oscar.y.chen@gmail.com>
2019-11-29 11:20:12 -0700

    changed default entities

--- src/Components/TagManagement/defaultTags.js
+++ src/Components/TagManagement/defaultTags.js
@@ -1,9 +1,9 @@
 import * as tagTypes from "./tagTypes";
 
 export const DEFAULTS = [
-  { id: "NEG_F", description: "NEGATION_FORWARD", color: "#fcba03", disabled: false, type: tagTypes.ENTITIES },
-  { id: "NEG_B", description: "NEGATION_BACKWARD", color: "#88f7af", disabled: false, type: tagTypes.ENTITIES },
-  { id: "NEG_BI", description: "NEGATION_BIDIRECTIONAL", color: "#9df283", disabled: false, type: tagTypes.ENTITIES },
+  { id: "NEG_F", description: "NEGATION_FORWARD", color: "#9df283", disabled: false, type: tagTypes.ENTITIES },
+  { id: "NEG_B", description: "NEGATION_BACKWARD", color: "#d3a3f7", disabled: false, type: tagTypes.ENTITIES },
+  { id: "NEG_BI", description: "NEGATION_BIDIRECTIONAL", color: "#fcba03", disabled: false, type: tagTypes.ENTITIES },
   { id: "CLOS_B", description: "CLOSURE_BUT", color: "#f277c3", disabled: false, type: tagTypes.ENTITIES },
   { id: "meds", description: "", color: "", disabled: false, type: tagTypes.SECTIONS },
   { id: "other_dx", description: "", color: "", disabled: false, type: tagTypes.SECTIONS },
