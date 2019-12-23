17cf6547cf953c03873bfe62422d927a076ba855
alexanderkrusina <alexander.krusina@ucalgary.ca>
2019-12-23 11:39:37 -0700

    annotate layouts

--- src/Pages/Annotate/layouts.js
+++ src/Pages/Annotate/layouts.js
@@ -1,32 +1,32 @@
 const defaultLayoutLg = [
   { w: 30, h: 7, x: 0, y: 0, i: "tagSelector" },
-  { w: 18, h: 7, x: 30, y: 0, i: "manageFiles" },
-  { w: 30, h: 35, x: 0, y: 7, i: "document" },
-  { w: 18, h: 35, x: 30, y: 7, i: "legend" }
+  { w: 10, h: 40, x: 38, y: 0, i: "manageFiles" },
+  { w: 30, h: 33, x: 0, y: 7, i: "document" },
+  { w: 8, h: 40, x: 30, y: 0, i: "legend" }
 ];
 const defaultLayoutMd = [
-  { w: 30, h: 7, x: 0, y: 0, i: "tagSelector" },
-  { w: 10, h: 7, x: 30, y: 0, i: "manageFiles" },
-  { w: 30, h: 35, x: 0, y: 7, i: "document" },
-  { w: 10, h: 35, x: 30, y: 7, i: "legend" }
+  { w: 25, h: 7, x: 0, y: 0, i: "tagSelector" },
+  { w: 8, h: 40, x: 32, y: 0, i: "manageFiles" },
+  { w: 25, h: 33, x: 0, y: 7, i: "document" },
+  { w: 7, h: 40, x: 25, y: 7, i: "legend" }
 ];
 const defaultLayoutSm = [
   { w: 16, h: 7, x: 0, y: 0, i: "tagSelector" },
-  { w: 8, h: 7, x: 16, y: 0, i: "manageFiles" },
-  { w: 16, h: 25, x: 0, y: 7, i: "document" },
-  { w: 8, h: 25, x: 16, y: 7, i: "legend" }
+  { w: 8, h: 22, x: 16, y: 0, i: "manageFiles" },
+  { w: 16, h: 31, x: 0, y: 7, i: "document" },
+  { w: 8, h: 16, x: 16, y: 7, i: "legend" }
 ];
 const defaultLayoutXs = [
   { w: 16, h: 7, x: 0, y: 0, i: "tagSelector" },
-  { w: 16, h: 4, x: 0, y: 7, i: "manageFiles" },
-  { w: 16, h: 20, x: 0, y: 24, i: "document" },
-  { w: 16, h: 20, x: 0, y: 11, i: "legend" }
+  { w: 16, h: 10, x: 0, y: 7, i: "manageFiles" },
+  { w: 16, h: 20, x: 0, y: 23, i: "document" },
+  { w: 16, h: 6, x: 0, y: 11, i: "legend" }
 ];
 const defaultLayoutXxs = [
   { w: 8, h: 7, x: 0, y: 0, i: "tagSelector" },
-  { w: 8, h: 4, x: 0, y: 7, i: "manageFiles" },
-  { w: 8, h: 15, x: 0, y: 21, i: "document" },
-  { w: 8, h: 10, x: 0, y: 11, i: "legend" }
+  { w: 8, h: 8, x: 0, y: 7, i: "manageFiles" },
+  { w: 8, h: 15, x: 0, y: 23, i: "document" },
+  { w: 8, h: 8, x: 0, y: 15, i: "legend" }
 ];
 
 export const defaultLayouts = {
