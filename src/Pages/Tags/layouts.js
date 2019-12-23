29f06d5ad6af844bbf668c28460d33b773fea5d2
alexanderkrusina <alexander.krusina@ucalgary.ca>
2019-12-23 11:11:16 -0700

    default layout for settings page

--- src/Pages/Tags/layouts.js
+++ src/Pages/Tags/layouts.js
@@ -1,22 +1,22 @@
 const defaultLayoutLg = [
-  { w: 7, h: 15, x: 0, y: 0, i: "tagList" },
-  { w: 2, h: 15, x: 7, y: 0, i: "generalSettings" }
+  { w: 8, h: 10, x: 0, y: 0, i: "tagList" },
+  { w: 3, h: 40, x: 8, y: 0, i: "generalSettings" }
 ];
 const defaultLayoutMd = [
-  { w: 7, h: 15, x: 0, y: 0, i: "tagList" },
-  { w: 2, h: 15, x: 7, y: 0, i: "generalSettings" }
+  { w: 7, h: 15, x: 0, y: 4, i: "tagList" },
+  { w: 10, h: 4, x: 0, y: 0, i: "generalSettings" }
 ];
 const defaultLayoutSm = [
-  { w: 7, h: 15, x: 0, y: 0, i: "tagList" },
-  { w: 2, h: 15, x: 7, y: 0, i: "generalSettings" }
+  { w: 7, h: 15, x: 0, y: 4, i: "tagList" },
+  { w: 10, h: 4, x: 0, y: 0, i: "generalSettings" }
 ];
 const defaultLayoutXs = [
-  { w: 7, h: 15, x: 0, y: 0, i: "tagList" },
-  { w: 2, h: 15, x: 7, y: 0, i: "generalSettings" }
+  { w: 7, h: 15, x: 0, y: 4, i: "tagList" },
+  { w: 10, h: 4, x: 0, y: 0, i: "generalSettings" }
 ];
 const defaultLayoutXxs = [
-  { w: 7, h: 15, x: 0, y: 0, i: "tagList" },
-  { w: 2, h: 15, x: 7, y: 0, i: "generalSettings" }
+  { w: 7, h: 15, x: 0, y: 4, i: "tagList" },
+  { w: 10, h: 4, x: 0, y: 0, i: "generalSettings" }
 ];
 
 export const defaultLayouts = {
